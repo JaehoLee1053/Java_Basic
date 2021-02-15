8d2a8b7ef6238c4e3e2cd96ce336d7fd7e6c2a53
JaehoLee1053 <67194430+JaehoLee1053@users.noreply.github.com>
2021-02-15 22:47:06 +0900

    Delete src directory

--- src/inheritance/CustomerTest.java
+++
@@ -1,8 +0,0 @@
-package inheritance;
-
-public class CustomerTest {
-	
-	Customer cusomerLee = new Customer(10010, "이순신");
-	
-
-}
--- src/practiceCustomer/NoteBook.java
+++
@@ -1,7 +0,0 @@
-package practiceCustomer;
-
-public abstract class NoteBook extends Computer {
-	public void typing() {
-		System.out.println("Notebook typing");
-	}
-}
