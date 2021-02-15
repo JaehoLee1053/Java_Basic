b12a151f6a0426b82c0e970a9123bdaa437262b4
JaehoLee1053 <dlwogh1053@gmail.com>
2021-02-16 00:35:53 +0900

    20210216_Update variable

---
+++ Chapter2/src/variable/IntegerTest.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class IntegerTest {
+
+	public static void main(String[] args) {
+
+		byte bs = -128;
+
+		System.out.println(bs);
+
+		// int iVal = 12345678900;
+		long lVal = 123456678900L; // long type은 식별자 L로 표기하여 8-byte임을 표기 
+
+		System.out.println(lVal);
+	}
+
+}
