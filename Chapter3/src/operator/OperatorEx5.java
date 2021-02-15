3d82a98d0312ae6fee7a1dbb5a0deaa7365016b7
JaehoLee1053 <67194430+JaehoLee1053@users.noreply.github.com>
2021-02-16 01:58:43 +0900

    Add files via upload
    20210216_Update operator

---
+++ Chapter3/src/operator/OperatorEx5.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class OperatorEx5 {
+
+	public static void main(String[] args) {
+
+		int num1 = 10;
+		int num2 = 20;
+
+		int max = (num1 > num2) ? num1 : num2;
+
+		System.out.println(max);
+	}
+
+}
